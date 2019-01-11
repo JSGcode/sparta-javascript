a8fa8b09953add779f9f94444bcae9ca280e2d4e
JSGcode <JMontesdeoca@spartaglobal.com>
2019-01-11 11:45:28 +0000

    Intial commit

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+//alert ("hello");
+
+console.log("Hi in console");
+var myNumber = 90;
+var myNumber1 =10;
+console.log(myNumber+" "+myNumber1);
+console.log(myNumber+myNumber1);
+console.log("number "+myNumber+" plus "+myNumber1+" equals "+(myNumber+myNumber1));
+console.log("number ", myNumber, " plus ", myNumber1, " equals ", (myNumber+myNumber1));
+
